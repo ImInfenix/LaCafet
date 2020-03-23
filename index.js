c7ade2daf3ea50ceb46205442462e8cb5ec3d502
ImInfenix <duncan.sourdin@etudiant.univ-rennes1.fr>
2020-03-23 17:03:35 +0100

    Added Files and some pain au chocolat

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'NjkxNTgwOTYxNzMyNjg5OTMw.XniDvQ.GcAh4buPHdDj76pCMqwKzZuroX4'
+const token = 'NjkxNTgwOTYxNzMyNjg5OTMw.XnjTqg.QFWF_vo3H3peBTn3HewAK7F0x5Y'
 
 bot.on('ready', () => {
   console.log('La Cafet, prête pour vous servir !');
@@ -8,27 +8,49 @@ bot.on('ready', () => {
 
 const painChocImgURL = 'https://cdn.discordapp.com/attachments/689045425654988832/691575282263392356/9k.png';
 const cafeImgURL = 'https://cdn.discordapp.com/attachments/689045425654988832/691575227477655632/Z.png';
-//const coca
-
+const cocaImgURL = 'https://cdn.discordapp.com/attachments/691583463090028607/691672063303483392/maxresdefault.jpg';
+const jambonBeurreImgURL = 'https://cdn.discordapp.com/attachments/691583463090028607/691673410010611742/th.png';
 
 bot.on('message', message => {
 
     if(message.author.bot) return;
 
     if (message.content.includes('café')) {
-      message.channel.send('Et un café pour ' + message.author.username +' !');
-      message.channel.send(cafeImgURL);
+      message.channel.send('Et un café pour ' + message.member.nickname +' !');
+      message.channel.send({files: [cafeImgURL]});
     }
 
     if (message.content.includes('pain au chocolat')) {
-      message.channel.send('Et un pain au chocolat pour ' + message.author.username +' !');
-      message.channel.send(painChocImgURL);
+      message.channel.send('Et un pain au chocolat pour ' + message.member.nickname +' !');
+      message.channel.send({files: [painChocImgURL]});
     }
 
     if(message.content.includes('chocolatine')) {
       message.channel.send('ON DIT PAIN AU CHOCOLAT CONNARD !!!');
     }
 
+    if (message.content.includes('coca')) {
+      message.channel.send('Et un coca pour ' + message.member.nickname +' !');
+      message.channel.send({files: [cocaImgURL]});
+    }
+
+    if(message.content.includes('jambon beurre')) {
+      message.channel.send('Et un jambon beurre pour ' + message.member.nickname +' !');
+      message.channel.send({files: [jambonBeurreImgURL]});
+    }
+
+    if(message.content.includes('commander'))
+    {
+      message.channel.send('========================================\n' +
+                          'Voici les différentes choses que vous pouvez commander : \n' +
+                          '\t- Café\n' +
+                          '\t- Coca\n' +
+                          '\t- Sandwich Jambon Beurre\n' +
+                          '\t- Pain au chocolat\n' +
+                          '========================================');
+    }
+
 });
 
+console.log("Logging !");
 bot.login(token);
